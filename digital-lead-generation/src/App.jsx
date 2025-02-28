729e4400cabcbb439fe4596d90ef80927c0fa9f1
DINESH-JUMANI <jumanidinesh981@gmail.com>
2025-02-28 09:16:39 +0530

    Added the welcome page

--- digital-lead-generation/src/App.jsx
+++ digital-lead-generation/src/App.jsx
@@ -1,12 +1,28 @@
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/routes';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Welcome from './pages/Welcome/Welcome';
+import UserForm from './pages/UserForm/UserForm';
+import AvatarCreation from './pages/AvatarCreation/AvatarCreation';
+import AvatarCustomization from './pages/AvatarCustomization/AvatarCustomization';
+import ThankYou from './pages/ThankYou/ThankYou';
 import { ThemeProvider } from './context/ThemeContext';
 import './App.css';
 
 function App() {
   return (
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <Router>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/user-form" element={<UserForm />} />
+            <Route path="/avatar-creation" element={<AvatarCreation />} />
+            <Route path="/avatar-customization" element={<AvatarCustomization />} />
+            <Route path="/thank-you" element={<ThankYou />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 }
